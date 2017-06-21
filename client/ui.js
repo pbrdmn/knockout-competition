343277193baaf60c34fcce57c015084577b5a015
Philip Boardman <dustykeyboard@brd.mn>
2017-06-21 22:43:35 +1000

    Slim down ui

--- client/ui.js
+++ client/ui.js
@@ -1,47 +1,20 @@
 const ui = {
-    elements: {},
+    E: {},
 
-    el: (id) => {
-        if (ui.elements[id]) return ui.elements[id]
-        return ui.elements[id] = document.getElementById(id)
-    },
+    el: (id) => (ui.E[id]) ? ui.E[id] : ui.E[id] = document.getElementById(id),
 
     displayWinner: (winner) => {
+        ui.clear()
         ui.el("winner").innerHTML = winner
-        ui.el("winner").className = "visible"
-        ui.el("progress").innerHTML = ""
     },
 
-    displayRound: (round) => {
-        const message = `Round ${(round + 1)}`
-        if (!ui.el("results")) return console.log(message)
-        const h2 = document.createElement("h2")
-        h2.setAttribute("id", `round_${round}`)
-        h2.className = "results_round"
-        h2.textContent = message
-        ui.el("results").appendChild(h2)
-    },
+    displayRound: (round) => console.log(`Round ${(round + 1)}`),
 
-    displayMatch: ({ winner, losers }) => {
-        const message = `${winner.name} <i>defeated</i> ${losers.map(team => team.name).join(", ")}`
-        if (!ui.el("results")) return console.log(message)
-        const p = document.createElement("p")
-        p.className = "results_match"
-        p.innerHTML = message
-        ui.el("results").appendChild(p)
-    },
+    displayMatch: ({ winner, losers }) => console.log(`${winner.name} defeated ${losers.map(team => team.name).join(", ")}`),
 
-    displayProgress: ({ completed, pending }) => {
-        ui.el("progress").innerHTML = Array(completed).fill('☒').concat( Array(pending).fill('☐') ).join('')
-    },
+    displayProgress: ({ completed, pending }) => ui.el("progress").innerHTML = Array(completed).fill('☒').concat( Array(pending).fill('☐') ).join(''),
 
-    displayError: (error) => {
-        ui.el("error").innerHTML = error
-        ui.el("error").className = "visible"
-    },
+    displayError: (error) => ui.el("error").innerHTML = error,
 
-    clear: () => {
-        ["winner", "error", "progress"].map(id => {ui.el(id).innerHTML = ""; ui.el(id).className = ""})
-        if (ui.el("results")) ui.el("results").innerHTML = ""
-    }
+    clear: () => ["winner", "error", "progress"].map(id => ui.el(id).innerHTML = "")
 }
