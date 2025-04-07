8ea4b5eb1b068a414b0bca2649a9e8552ad42af5
Philip Boardman <github@brd.mn>
2025-04-07 21:33:22 +1000

    App client utilises 6 concurrent connections

--- client/client.js
+++ client/client.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         // decouple the networking from the business login
         http: new HTTP({
             // rateLimit: 5 // max requests per second
-            connectionsLimit: 4 // max simultaneous connections
+            connectionsLimit: 6 // max simultaneous connections - https://stackoverflow.com/a/985704
         }),
         // debug: true // Enable debug logging
     })
